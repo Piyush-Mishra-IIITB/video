7e83454d2b89c287df36cae53729ac480b7ac9c5
Piyush mishra <piyushmishra8982@gmail.com>
2026-02-08 19:48:09 +0530

    added AuthContext for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import LandingPage from "./pages/landing.jsx";
 import Authentication from "./pages/authentication.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+    <AuthProvider>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path='/auth' element={<Authentication/>}/>
       </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
