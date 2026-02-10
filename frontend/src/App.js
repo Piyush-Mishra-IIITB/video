1f864633d8399ee6598ec40ad78e88c2bfc9e5f8
Piyush mishra <piyushmishra8982@gmail.com>
2026-02-10 18:20:49 +0530

    added somepart of socket-io

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/home.jsx";
 import LandingPage from "./pages/landing.jsx";
 import Authentication from "./pages/authentication.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
-
+import VideoMeet from "./pages/VideoMeet.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +12,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path='/auth' element={<Authentication/>}/>
+        <Route path='/:url' element={<VideoMeet/>}/>
       </Routes>
       </AuthProvider>
     </BrowserRouter>
---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,166 @@
+
+import "../Styles/videoComponents.css";
+import React, { useEffect, useRef, useState } from 'react'
+import io from "socket.io-client";
+import { Badge, IconButton, TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import VideocamOffIcon from '@mui/icons-material/VideocamOff'
+
+import CallEndIcon from '@mui/icons-material/CallEnd'
+import MicIcon from '@mui/icons-material/Mic'
+import MicOffIcon from '@mui/icons-material/MicOff'
+import ScreenShareIcon from '@mui/icons-material/ScreenShare';
+import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
+import ChatIcon from '@mui/icons-material/Chat'
+
+
+
+const server_url = "http://localhost:8080";
+
+
+
+var connections = {};
+
+const peerConfigConnections = {
+    "iceServers": [
+        { "urls": "stun:stun.l.google.com:19302" }
+    ]
+}
+
+export default function VideoMeetComponent() {
+
+    var socketRef = useRef();
+    let socketIdRef = useRef();
+    const connectToSocket = () => {
+    socketRef.current = io(server_url);
+    console.log("Socket connected");
+};
+
+    let localVideoref = useRef();
+
+    let [videoAvailable, setVideoAvailable] = useState(true);
+
+    let [audioAvailable, setAudioAvailable] = useState(true);
+
+    let [video, setVideo] = useState([]);
+
+    let [audio, setAudio] = useState();
+
+    let [screen, setScreen] = useState();
+
+    let [showModal, setModal] = useState(true);
+
+    let [screenAvailable, setScreenAvailable] = useState();
+
+    let [messages, setMessages] = useState([])
+
+    let [message, setMessage] = useState("");
+
+    let [newMessages, setNewMessages] = useState(3);
+
+    let [askForUsername, setAskForUsername] = useState(true);
+
+    let [username, setUsername] = useState("");
+
+    const videoRef = useRef([])
+
+    let [videos, setVideos] = useState([])
+
+    // TODO
+    // if(isChrome() === false) {
+
+
+    // }
+
+    const getPermissions = async () => {
+        try {
+            const videoPermission = await navigator.mediaDevices.getUserMedia({ video: true });
+            if (videoPermission) {
+                setVideoAvailable(true);
+                console.log('Video permission granted');
+            } else {
+                setVideoAvailable(false);
+                console.log('Video permission denied');
+            }
+
+            const audioPermission = await navigator.mediaDevices.getUserMedia({ audio: true });
+            if (audioPermission) {
+                setAudioAvailable(true);
+                console.log('Audio permission granted');
+            } else {
+                setAudioAvailable(false);
+                console.log('Audio permission denied');
+            }
+
+            if (navigator.mediaDevices.getDisplayMedia) {
+                setScreenAvailable(true);
+            } else {
+                setScreenAvailable(false);
+            }
+
+            if (videoAvailable || audioAvailable) {
+                const userMediaStream = await navigator.mediaDevices.getUserMedia({ video: videoAvailable, audio: audioAvailable });
+                if (userMediaStream) {
+                    window.localStream = userMediaStream;
+                    if (localVideoref.current) {
+                        localVideoref.current.srcObject = userMediaStream;
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getPermissions();
+       
+
+    },[]);
+    const getUserMediaSuccess = (stream) => {
+    window.localStream = stream;
+    if (localVideoref.current) {
+        localVideoref.current.srcObject = stream;
+    }
+};
+
+     let getUserMedia = () => {
+        if ((video && videoAvailable) || (audio && audioAvailable)) {
+            navigator.mediaDevices.getUserMedia({ video: video, audio: audio })
+                .then(getUserMediaSuccess)
+                .then((stream) => { })
+                .catch((e) => console.log(e))
+        } else {
+            try {
+                let tracks = localVideoref.current.srcObject.getTracks()
+                tracks.forEach(track => track.stop())
+            } catch (e) { }
+        }
+    }
+     useEffect(() => {
+        if (video !== undefined && audio !== undefined) {
+            getUserMedia();
+            console.log("SET STATE HAS ", video, audio);
+
+        }
+
+
+    }, [video, audio]);
+    let getMedia=()=>{
+        setAudio(audioAvailable);
+        setVideo(videoAvailable);
+        connectToSocket();
+    }
+    return(<>
+    <div>
+        <h2>Enter into Lobby</h2>
+        <TextField></TextField>
+        <Button variant="contained" onClick={connectToSocket}>Contained</Button>
+    </div>
+     <div>
+<video ref={localVideoref} autoPlay muted></video>
+      </div>
+    </>);
+
+}
